c28921d6dc1aa4d6dd91c118642a3331fa676294
marctost <marctost@visitor-32391400.dyndns.cern.ch>
2018-07-06 09:16:51 +0200

    made more descriptive names

---
+++ make_rate_plots.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+root -q -l 'excel_stuff.C("ALCT_no")'
+root -q -l 'excel_stuff.C("ALCT_top")'
+root -q -l 'excel_stuff.C("ALCT_bottom")'
+
+root -q -l 'excel_stuff.C("CLCT_no")'
+root -q -l 'excel_stuff.C("CLCT_top")'
+root -q -l 'excel_stuff.C("CLCT_bottom")'
+
+root -q -l 'excel_stuff.C("LCT_no")'
+root -q -l 'excel_stuff.C("LCT_top")'
+root -q -l 'excel_stuff.C("LCT_bottom")'
